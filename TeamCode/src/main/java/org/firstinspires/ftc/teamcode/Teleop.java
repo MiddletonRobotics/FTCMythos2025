8e74183375c680ad99f841a3ea12a20c2422c16d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2024-11-06 22:07:30 -0500

    zz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,19 +6,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name= "Telop")
+@TeleOp(name= "Teleop")
 public class Teleop extends OpMode {
     private DcMotor FrontLeft;
     private DcMotor FrontRight;
     private DcMotor BackLeft;
     private DcMotor BackRight;
-
+    private DcMotor ViperMotor;
     private Servo Claw;
     private Servo rollerClaw;
-
+    private Servo Arm;
     private boolean aButtonPreviousState;
     private boolean bButtonPreviousState;
     private boolean xButtonPreviousState;
+    private boolean leftBumperButtonPreviousState;
+    private boolean rightBumperButtonPreviousState;
+    private boolean aButtonPreviousStateg2;
+    private boolean bButtonPreviousStateg2;
     private boolean SlowMode;
 
     @Override
@@ -27,13 +31,16 @@ public class Teleop extends OpMode {
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         BackRight = hardwareMap.get(DcMotor.class, "BackRight");
-
+        ViperMotor = hardwareMap.get(DcMotor.class, "ViperMotor");
         Claw = hardwareMap.get(Servo.class, "Claw");
         Claw.setDirection(Servo.Direction.REVERSE);
-
+        Arm = hardwareMap.get(Servo.class, "Arm");
+        Arm.setDirection(Servo.Direction.FORWARD);
+        /*
         rollerClaw = hardwareMap.get(Servo.class, "rollerClaw");
-        rollerClaw.setDirection(Servo.Direction.REVERSE);
 
+        rollerClaw.setDirection(Servo.Direction.REVERSE);
+        */
         FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         FrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -43,10 +50,15 @@ public class Teleop extends OpMode {
         FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ViperMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         aButtonPreviousState = false;
         bButtonPreviousState = false;
         xButtonPreviousState = false;
+        leftBumperButtonPreviousState = false;
+        rightBumperButtonPreviousState = false;
+        aButtonPreviousStateg2 = false;
+        bButtonPreviousStateg2 = false;
         SlowMode = false;
 
         telemetry.addData("Status", "Initialized");
@@ -59,7 +71,8 @@ public class Teleop extends OpMode {
     public void start(){
         resetRuntime();
         Claw.setPosition(0.0);
-        rollerClaw.setPosition(1.0);
+        //rollerClaw.setPosition(1.0);
+        Arm.setPosition(0.0);
     };
 
     @Override
@@ -78,14 +91,30 @@ public class Teleop extends OpMode {
         } else if (gamepad1.b && bButtonPreviousState) {
             Claw.setPosition(0.55);
         }
-
+        if(gamepad1.left_bumper && !leftBumperButtonPreviousState) {
+            Arm.setPosition(0.06);
+        } else if (gamepad1.right_bumper && !rightBumperButtonPreviousState) {
+            Arm.setPosition(0.6);
+        }
         if(gamepad1.x && !xButtonPreviousState) {
             SlowMode = !SlowMode;
+        }
+        if(gamepad2.a && !aButtonPreviousStateg2) {
+            ViperMotor.setTargetPosition(0);
+            ViperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if (gamepad2.b && !bButtonPreviousStateg2){
+            ViperMotor.setTargetPosition(2000);
+            ViperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         }
 
         aButtonPreviousState = gamepad1.a;
         bButtonPreviousState = gamepad1.b;
         xButtonPreviousState = gamepad1.x;
+        leftBumperButtonPreviousState = gamepad1.left_bumper;
+        rightBumperButtonPreviousState = gamepad1.right_bumper;
+        aButtonPreviousStateg2 = gamepad2.a;
+        bButtonPreviousStateg2 =  gamepad2.b;
 
         double[] thetaSpeeds = {
                 (drive + strafe + rotation),
