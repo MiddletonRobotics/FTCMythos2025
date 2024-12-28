fbcc48f05cc49217ebe98ff2d92fefc69b6668c5
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2024-12-28 18:43:14 -0500

    automatic transfer working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeSmapleThenRetract.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+public class IntakeSmapleThenRetract extends CommandBase {
+    private final ElevatorSubsystem elevatorSubsystem;
+    private final IntakeSubsystem intakeSubsystem;
+
+    public IntakeSmapleThenRetract(ElevatorSubsystem elevatorSubsystem, IntakeSubsystem intakeSubsystem) {
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.intakeSubsystem = intakeSubsystem;
+
+        addRequirements(elevatorSubsystem, intakeSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        intakeSubsystem.intakeToPosition(
+                intakeSubsystem.getExtensionState(),
+                intakeSubsystem.getArmState(),
+                intakeSubsystem.getWristState(),
+                IntakeSubsystem.ClawState.CLOSE_CLAW
+        );
+
+        intakeSubsystem.intakeTimer.resetTimer();
+    }
+
+    @Override
+    public void execute() {
+        if (intakeSubsystem.intakeTimer.getElapsedTimeSeconds() >= 0.25 && intakeSubsystem.intakeTimer.getElapsedTimeSeconds() < 0.5) {
+            intakeSubsystem.intakeToPosition(
+                    intakeSubsystem.getExtensionState(),
+                    IntakeSubsystem.ArmState.TRANSFER,
+                    IntakeSubsystem.WristState.NORMAL,
+                    intakeSubsystem.getClawState()
+            );
+        } else if (intakeSubsystem.intakeTimer.getElapsedTimeSeconds() >= 0.5 && intakeSubsystem.intakeTimer.getElapsedTimeSeconds() < 1.5) {
+            intakeSubsystem.intakeToPosition(
+                    IntakeSubsystem.ExtensionState.STORED,
+                    intakeSubsystem.getArmState(),
+                    intakeSubsystem.getWristState(),
+                    intakeSubsystem.getClawState()
+            );
+
+            elevatorSubsystem.elevatorToPosition(ElevatorSubsystem.LiftState.CLEARENCE);
+        } else if (intakeSubsystem.intakeTimer.getElapsedTimeSeconds() >= 1.5 && intakeSubsystem.intakeTimer.getElapsedTimeSeconds() < 2.25) {
+            elevatorSubsystem.elevatorToPosition(ElevatorSubsystem.LiftState.RETRACTED);
+            elevatorSubsystem.manipulatorToPosition(
+                    ElevatorSubsystem.ArmState.TRANSFER,
+                    ElevatorSubsystem.WristState.TRANSFER,
+                    ElevatorSubsystem.ClawState.OPEN_CLAW
+            );
+        } else if (intakeSubsystem.intakeTimer.getElapsedTimeSeconds() >= 2.25) {
+            elevatorSubsystem.manipulatorToPosition(
+                    elevatorSubsystem.getArmState(),
+                    elevatorSubsystem.getWristState(),
+                    ElevatorSubsystem.ClawState.CLOSE_CLAW
+            );
+
+            intakeSubsystem.intakeToPosition(
+                    intakeSubsystem.getExtensionState(),
+                    intakeSubsystem.getArmState(),
+                    intakeSubsystem.getWristState(),
+                    IntakeSubsystem.ClawState.PARTIALLY_OPEN_CLAW
+            );
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return intakeSubsystem.intakeTimer.getElapsedTimeSeconds() > 2.5;
+    }
+}
