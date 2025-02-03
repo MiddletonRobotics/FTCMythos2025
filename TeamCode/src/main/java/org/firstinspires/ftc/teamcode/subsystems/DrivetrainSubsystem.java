07427fb82ee4ca91233642939bc29eaaf4ae28b5
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-02-02 22:13:32 -0500

    distance sensor implemenation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DrivetrainSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DrivetrainSubsystem.java
@@ -3,17 +3,22 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.RobotDrive;
 import com.arcrobotics.ftclib.geometry.Vector2d;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 public class DrivetrainSubsystem extends SubsystemBase {
     private MecanumDrive mecanumDrive;
+    private Rev2mDistanceSensor frontDistanceSensor, rearDistanceSensor;
 
     public DrivetrainSubsystem(HardwareMap aHardwareMap) {
         mecanumDrive = new MecanumDrive(aHardwareMap);
+        frontDistanceSensor = aHardwareMap.get(Rev2mDistanceSensor.class, "frontDistanceSensor");
+        rearDistanceSensor = aHardwareMap.get(Rev2mDistanceSensor.class, "rearDistanceSensor");
     }
 
     public void resetHeading(IMU imu) {
@@ -35,5 +40,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     public void driveRobotCentric(double strafeSpeed, double forwardSpeed, double rotationalSpeed) {
         driveRobotCentric(strafeSpeed, forwardSpeed, rotationalSpeed, false);
+    }
+
+    public double getFrontDistance() {
+        return frontDistanceSensor.getDistance(DistanceUnit.MM);
+    }
+
+    public double getRearDistance() {
+        return rearDistanceSensor.getDistance(DistanceUnit.MM);
     }
 }
